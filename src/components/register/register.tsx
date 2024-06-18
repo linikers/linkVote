ec422c7b39f0fdb4d80e6ada5a04e652856926d4
linikers <linikers@hotmail.com>
2024-06-18 10:54:21 -0300

    fix: input register

--- src/components/register/register.tsx
+++ src/components/register/register.tsx
@@ -1,21 +1,25 @@
 import { Button, FormControl, Grid, TextField } from "@material-ui/core"
-import { FC, FormEvent, useState } from "react"
+import React, { FC, FormEvent, useState } from "react"
 
 export const Register: FC = () => {
 
     const [formData, setFormData] = useState({name: "", work: "",})
 
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name,  value } = e.target;
+        setFormData({ 
+            ...formData, 
+            [name]: value,
+            // [work]: value,
+        })
+    }
     const handleRegister = (e: React.ChangeEvent<HTMLInputElement>) => {
         e.preventDefault()
 
-        const { name, work } = e.target;
         try {
             
-            setFormData({ 
-                ...formData, 
-                name: name.value,
-                work: work.value,
-            })
+ 
             localStorage.setItem("dataUser", JSON.stringify(formData))
             alert("Registrado com sucesso!")
         } catch (error) {
@@ -24,17 +28,17 @@ export const Register: FC = () => {
         }
     }
     return (
-    <Grid container spacing={2}>
-        <FormControl>
+    <Grid container spacing={2}  onSubmit={handleRegister}>
+        <FormControl fullWidth>
 
-        <Grid item xs={12}>
-            <TextField label="Nome" value={name} onChange={handleRegister}  fullWidth/>
+        <Grid item xs={12} >
+            <TextField label="Nome" value={formData.name} color="secondary" onChange={handleInputChange}  fullWidth/>
         </Grid>
         <Grid item xs={12}>
-            {/* <TextField label="Estudio" value={work} onChange={handleRegister}/> */}
+            <TextField label="Estudio" value={formData.work} onChange={handleRegister}/>
         </Grid>
         <Grid>
-            {/* <Button variant="contained" color="primary" onClick={handleRegister}>Salvar</Button> */}
+            <Button variant="contained" color="primary" type="submit">Salvar</Button>
         </Grid>
         </FormControl>
     </Grid>
