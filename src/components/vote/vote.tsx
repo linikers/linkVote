d534b0bc51501403286e76dcd57d0b5612bc8804
linikers <linikers@hotmail.com>
2024-06-18 16:35:01 -0300

    fix: tela votacao

--- src/components/vote/vote.tsx
+++ src/components/vote/vote.tsx
@@ -1,8 +1,9 @@
-import { Typography } from "@material-ui/core"
+import { Button, Typography } from "@material-ui/core"
+import { Grid } from "@mui/material"
 import { FC, useEffect, useState } from "react"
 
 export const Vote: FC = () => {
-    const [users, setUsers] = useState([])
+    const [users, setUsers] = useState<{ name: string; work: string}[]>([])
 
     useEffect(()=> {
         const storedUsers = localStorage.getItem("formData")
@@ -17,7 +18,19 @@ const handleVote = (userName: string) => {
         <form>
         <Typography variant="h4">Vote Agora</Typography>
         <Grid>
-            
+            {users.length > 0 ? (
+                users.map((user, index) => (
+
+                    <Grid>
+                        <Typography>{user.name}</Typography>
+                        <Typography>{user.work}</Typography>
+                        <Button variant="contained" color="primary" onClick={()=> handleVote(user.name)} style={{ marginTop: "0.5rem"}}>Votar</Button>
+                    </Grid>
+                ))
+                ) : (
+                    <Typography variant="body1">Nenhum participante cadastrado</Typography>
+                )
+            }
         </Grid>
         </form>
     )
