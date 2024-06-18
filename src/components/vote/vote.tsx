07cb98e74882fd21e715d3ebaec73a06fa0a506d
linikers <linikers@hotmail.com>
2024-06-18 16:26:36 -0300

    fix: register

--- src/components/vote/vote.tsx
+++ src/components/vote/vote.tsx
@@ -1,11 +1,24 @@
 import { Typography } from "@material-ui/core"
-import { FC } from "react"
+import { FC, useEffect, useState } from "react"
 
 export const Vote: FC = () => {
+    const [users, setUsers] = useState([])
 
+    useEffect(()=> {
+        const storedUsers = localStorage.getItem("formData")
+        if(storedUsers) {
+            setUsers(JSON.parse(storedUsers))
+        }
+    }, [])
+const handleVote = (userName: string) => {
+    alert(`Voce votou em ${userName}`)
+}
     return(
         <form>
-        <Typography>Vote Agora</Typography>
+        <Typography variant="h4">Vote Agora</Typography>
+        <Grid>
+            
+        </Grid>
         </form>
     )
 }
