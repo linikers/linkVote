bc226678d3354a77f00ada10d5267d18a64a71b0
linikers <linikers@hotmail.com>
2024-07-02 11:15:23 -0300

    conf blob

--- API/index.js
+++ API/index.js
@@ -1,21 +1,40 @@
 const express = require('express');
 const app = express();
 
+// app.use(express.json())
+const { BlobServiceClient } = require('@vercel/blob')
+const blobServiceClient = new BlobServiceClient({
+    token: process.eventNames.	
+    BLOB_READ_WRITE_TOKEN,
+})
 app.use(express.json())
-const tempDataBase = {}
-
-app.post('/api/save', (request, response) => {
-    const {key, value} = request.body
-    tempDataBase[key] = value;
+app.post('/api/save', async (request, response)=> {
+    const { key, value } = request.body;
+    const blob = await blobServiceClient.createBlob(`data/${key}`, value);
     response.json({ success: true })
 })
-app.get('/api/get', (req, res) => {
-    const {key} = request.query
-    const value = tempDataBase[key];
-    if(value) {
-        response.json(value)
+// app.post('/api/save', (request, response) => {
+//     const {key, value} = request.body
+//     tempDataBase[key] = value;
+//     response.json({ success: true })
+// })
+app.get('/api/get', async(request, response) => {
+    const { key } = request.query;
+    const blob = await blobServiceClient.getBlob(`data/${key}`);
+    if(blob) {
+        const data = await blob.text();
+        response.json(data)
     } else {
         response.status(404).json({ error: `Dados não encontrados`})
     }
 })
+// app.get('/api/get', (req, res) => {
+//     const {key} = request.query
+//     const value = tempDataBase[key];
+//     if(value) {
+//         response.json(value)
+//     } else {
+//         response.status(404).json({ error: `Dados não encontrados`})
+//     }
+// })
 module.exports = app
