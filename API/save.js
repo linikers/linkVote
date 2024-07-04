d45f5fc4a7cd02d139da417642c722f7998cbee5
linikers <linikers@hotmail.com>
2024-07-04 17:10:43 -0300

    blob teste d novo

--- API/save.js
+++ API/save.js
@@ -1,21 +1,19 @@
 import { Blob } from '@vercel/blob';
 
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
+  if (req.method === 'POST') {
+    const { key, value } = req.body;
+
     try {
       const blob = new Blob(process.env.BLOB_READ_WRITE_TOKEN);
-      const data = await blob.get('competidores/votes.json');
+      await blob.put(`competidores/${key}.json`, JSON.stringify(value));
 
-      if (data) {
-        res.status(200).json(JSON.parse(data.toString()));
-      } else {
-        res.status(404).json({ success: false, error: 'Data not found' });
-      }
+      res.status(200).json({ success: true });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ success: false, error: error.message });
+      res.status(500).json({ success: false, error: 'Erro ao salvar os dados' });
     }
   } else {
-    res.status(405).json({ success: false, error: 'Method Not Allowed' });
+    res.status(405).json({ success: false, error: 'Método não permitido' });
   }
 }
