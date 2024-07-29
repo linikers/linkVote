8296650ebdd98ee5557e95a5e3b5e48d26a731ce
linikers <linikers@hotmail.com>
2024-07-29 09:28:59 -0300

    new base

--- API/save.js
+++ API/save.js
@@ -1,27 +1,50 @@
-import { put } from '@vercel/blob';
-import dotenv from 'dotenv';
+import { Pool } from "@vercel/postgres";
 
-dotenv.config();
+ const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+ });
 
-const blobServiceClient = {
-  token: process.env.BLOB_READ_WRITE_TOKEN,
-};
-
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
+ export default async (request, response) => {
+  if(request.method == 'POST') {
+    const { name, work, votes } = req.body;
+    
     try {
-      const { key, value } = req.body;
-      const blob = await put(`competidores/${key}.json`, Buffer.from(JSON.stringify(value)), {
-        contentType: 'application/json',
-        access: 'public',
-        token: blobServiceClient.token,
-      });
-      res.status(200).json({ success: true, blob });
+      const result = await pool.query(
+        'INSERT INTO competidore(name, work, votes) VALUES() RETURNING *',
+        [name, work, votes]
+      );
+      response.status(201).json(result.rows[0])
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ success: false, error: error.message });
+      response.status(500).json({ error: 'Erro ao cadastrar competidor'})
     }
   } else {
-    res.status(405).json({ success: false, error: 'Method Not Allowed' });
+    // response
   }
-}
+ } 
+// import { put } from '@vercel/blob';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const blobServiceClient = {
+//   token: process.env.BLOB_READ_WRITE_TOKEN,
+// };
+
+// export default async function handler(req, res) {
+//   if (req.method === 'POST') {
+//     try {
+//       const { key, value } = req.body;
+//       const blob = await put(`competidores/${key}.json`, Buffer.from(JSON.stringify(value)), {
+//         contentType: 'application/json',
+//         access: 'public',
+//         token: blobServiceClient.token,
+//       });
+//       res.status(200).json({ success: true, blob });
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ success: false, error: error.message });
+//     }
+//   } else {
+//     res.status(405).json({ success: false, error: 'Method Not Allowed' });
+//   }
+// }
